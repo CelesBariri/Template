53a66cb7d0fdba17ae81b580928031d5a7436a1f
CelesBariri <rafael.celes.gf@gmail.com>
2023-09-22 15:30:01 -0300

    Projeto finalizado

--- src/components/Produto.jsx
+++ src/components/Produto.jsx
@@ -1,10 +1,11 @@
-import { Card, CardActions, CardContent, CardMedia, Typography, Button, Link } from "@mui/material"
+import { Card, CardActions, CardContent, CardMedia, Typography, Button, Link, Grid } from "@mui/material"
 
 function Produto(props) {
   return (
-    <Card sx={{ maxWidth: 345 }} key={props.key}>
+    <Grid item lg={3} md={6} sm={12} xs={12}>
+    <Card sx={{ minHeight: "300px", maxHeight: "300px" }} key={props.key}>
         <CardMedia 
-            sx={{ height: 140 }}
+            sx={{ height: 213 }}
             image={props.imagem}
             title={props.titulo}
         />
@@ -25,6 +26,7 @@ function Produto(props) {
             </Button>
         </CardActions>
     </Card>
+    </Grid>
   )
 }
 
